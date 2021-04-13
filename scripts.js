6c6263aaf78070e05eaaee1c534deaebd580b4d8
Sérgio Lucena <slucenadecarvalho@gmail.com>
2021-04-13 13:29:41 -0300

    Css do modal e criação de novas ideias

--- scripts.js
+++ scripts.js
@@ -3,6 +3,17 @@ function onOff(){
     .querySelector("#modal")
     .classList // acessar a lista de classes 
     .toggle("hide") // alternancia 
+
+    document
+    .querySelector("body")
+    .classList // acessar a lista de classes 
+    .toggle("hideScroll") // alternancia (tirar e botar)
+
+    document // um scroll so no modal 
+    .querySelector("#modal")
+    .classList
+    .toggle("addScroll")
+
 }
 
 
